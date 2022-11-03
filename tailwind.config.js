71bb07fbde5fdbe8807dadc3399f6567d51ad620
Richard So <dev.richard.so@gmail.com>
2022-11-04 10:16:52 +1300

    installed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./server/public/**/*.{html,js}'],
   theme: {
     extend: {},
   },
