36d59d6ab1bdb6464696dc610a0c404ec900f4a0
elvin <elvintern@gmail.com>
2022-11-04 11:52:59 +1300

    make randomising img props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import Round from './Round'
 
-// import data from '../../data/facilitators'
-// import iconData from '../../server/public/icons'
-// import soundData from '../../server/public/sounds'
-import Main from './Main'
-
+import data from '../../data/facilitators'
 
 function App() {
   function shuffle(array) {
@@ -34,18 +30,16 @@ function App() {
   const round1Contestants = [facilitators[0], facilitators[1]]
   const round2Contestants = [facilitators[2], facilitators[3]]
 
-  console.log(round1Contestants, round2Contestants)
+  // console.log(round1Contestants, round2Contestants)
   const [finalist, setFinalist] = useState([])
   return (
     <>
-
       {/* This 'main' div is only for styling (so we can use flexbox) */}
       <div className="main">
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/round" element={<Round />} />
+          <Route path="/round" element={<Round data={round1Contestants} />} />
         </Routes>
-
       </div>
     </>
   )
