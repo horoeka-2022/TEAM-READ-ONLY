d2261b02310aadfd53a4114ba5c16330c9ab6414
Richard So <dev.richard.so@gmail.com>
2022-11-04 11:13:59 +1300

    randomly pick contestants for round 1 and 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Main from './main'
-
-// import continentData from '../../data/continents'
+import Main from './Main'
+import data from '../../data/facilitators'
 
 function App() {
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+
+      // And swap it with the current element.
+      ;[array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ]
+    }
+
+    return array
+  }
+
+  const facilitators = shuffle(data)
+  const round1Contestants = [facilitators[0], facilitators[1]]
+  const round2Contestants = [facilitators[2], facilitators[3]]
+
+  console.log(round1Contestants, round2Contestants)
+  const [finalist, setFinalist] = useState([])
   return (
     <>
       <div className="flex justify-center">
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 function Main() {
   return (
     <>
-      <h2> this is the main page</h2>
       <button>START</button>
     </>
   )
