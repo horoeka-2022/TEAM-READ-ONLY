77f254ffb703bb12b09a293092016176b326fb5a
Richard So <dev.richard.so@gmail.com>
2022-11-03 18:56:58 +1300

    add images & installed tailwindcss

---
+++ data/facilitators.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    ahmad: {
+      name: 'Ahmad',
+      profileImage: '/images/ahmad.png',
+      celeb: {
+        femaleImage: '/images/ahmad-female.png',
+        maleImage: '/images/ahmad-male.png',
+        best: '/images/ahmad-best.png',
+      },
+    },
+  },
+  {
+    alex: {
+      name: 'Alex',
+      profileImage: '/images/alex.png',
+      celeb: {
+        femaleImage: '/images/alex-female.png',
+        maleImage: '/images/alex-male.png',
+        best: '/images/alex-best.png',
+      },
+    },
+  },
+  {
+    clem: {
+      name: 'Clementine',
+      profileImage: '/images/clem.png',
+      celeb: {
+        femaleImage: '/images/clem-female.png',
+        maleImage: '/images/clem-male.png',
+        best: '/images/clem-best.png',
+      },
+    },
+  },
+  {
+    johnny: {
+      name: 'Johnny',
+      profileImage: '/images/johnny.png',
+      celeb: {
+        femaleImage: '/images/johnny-female.png',
+        maleImage: '/images/johnny-male.png',
+        best: '/images/johnny-best.png',
+      },
+    },
+  },
+]
---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import Nav from './Nav'
+import Home from './Home'
+import Continent from './Continent'
+import Country from './Country'
+
+// import continentData from '../../data/continents'
+
+function App() {
+  return (
+    <>
+      <div className="title">
+        <img
+          src="/images/color_earth.gif"
+          alt="A coloured globe of the earth spinning around on its axis"
+        />
+        <h1>Navigating the worldwide routes</h1>
+      </div>
+      {/* This 'main' div is only for styling (so we can use flexbox) */}
+      <div className="main">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="continents/:name" element={<Continent />} />
+          <Route path="/continents/:name/:code" element={<Country />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
